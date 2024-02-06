be861ecdc9a71b0e55b061fd882b64f146511772
Stygian84 <nicholasgandhi84@gmail.com>
2024-02-07 01:00:01 +0800

    Init Commit

---
+++ app.js
@@ -0,0 +1,180 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+require("dotenv").config();
+
+const port = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  sslmode: "require",
+});
+
+app.get("/api/data/:tableName", async (req, res) => {
+  const { tableName } = req.params;
+
+  try {
+    // Use parameterized query to avoid SQL injection
+    const result = await pool.query(`SELECT * FROM public."${tableName}"`);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/query/:sqlQuery", async (req, res) => {
+  const { sqlQuery } = req.params;
+
+  try {
+    // Use parameterized query to avoid SQL injection
+    const result = await pool.query(sqlQuery); //http://localhost:3001/api/data/SELECT%20*%20FROM%20public."PlantData"
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+/*
+list query needed for each page
+ROW : rowdata + status
+Status : rowdata
+PlantDetails : Plantdata
+plantdetails graph -> plantdata (plantid,timestamp, value)
+*/
+
+//Row Page (display each row and its overall status)
+app.get("/api/row", async (req, res) => {
+  try {
+    const query = {
+      text: `SELECT RowID, Status
+      FROM public."RowData"
+      ORDER BY RowID ASC;
+      `,
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//Status Page (display specific row status)
+app.get("/api/status", async (req, res) => {
+  try {
+    const query = {
+      text: `SELECT *
+      FROM public."RowData"
+      ORDER BY RowID ASC;
+      `,
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/status/:rowId", async (req, res) => {
+  try {
+    const { rowId } = req.params;
+    const query = {
+      text: `SELECT *
+            FROM public."RowData"
+            WHERE RowID = $1
+            ORDER BY RowID ASC`,
+      values: [rowId],
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// misc
+app.get("/api/plant", async (req, res) => {
+  try {
+    const query = {
+      text: `SELECT *
+      FROM public."PlantData"
+      ORDER BY RowID ASC;
+      `,
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//PLantDetails (display specific rowid plants latest timestamp details)
+// eg http://localhost:3001/api/plant/1
+app.get("/api/plant/:rowId", async (req, res) => {
+  try {
+    const { rowId } = req.params;
+    const query = {
+      text: `SELECT *
+          FROM public."PlantData" pd
+          JOIN (
+          SELECT PlantID, MAX(Timestamp) AS latest_timestamp
+          FROM public."PlantData"
+          WHERE RowID = $1
+          GROUP BY PlantID
+          ) AS latest ON pd.PlantID = latest.PlantID AND pd.Timestamp = latest.latest_timestamp
+          ORDER BY pd.PlantID ASC;
+      
+      `,
+      values: [rowId],
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//PlantDetailsGraph (display specific plant id in a specific rowid with specific data (eg humidity) and all timestamp)
+// e.g. http://localhost:3001/api/plant/1/1/humidity
+app.get("/api/plant/:rowId/:plantId/:property", async (req, res) => {
+  try {
+    const { rowId, plantId, property } = req.params;
+    const query = {
+      text: `SELECT timestamp, ${property}
+      FROM public."PlantData"
+      WHERE RowID = $1 AND PlantID = $2
+      ORDER BY timestamp ASC`,
+      values: [rowId, plantId],
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Start the server
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
